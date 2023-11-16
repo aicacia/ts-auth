3ed8f7e164b360d6064310d3c75a3ab4e5cc1a24
nathanfaucett <nathanfaucett@users.noreply.github.com>
2023-11-16 13:04:17 +0000

    deploy: dfc79beb41a7c02ee40d37908f8cd21fb917630e

--- internal/immutable/chunks/paths.b340fad0.js
+++
@@ -1,1 +0,0 @@
-var s,e;const c=(e=(s=globalThis.__sveltekit_1hccnde)==null?void 0:s.base)!=null?e:"";var a,t;const l=(t=(a=globalThis.__sveltekit_1hccnde)==null?void 0:a.assets)!=null?t:c;export{l as a,c as b};
--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
 const n = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u = [
-  n + "/internal/immutable/entry/app.fad86364.js",
+  n + "/internal/immutable/entry/app.c406c489.js",
   n + "/internal/immutable/assets/0.00ad9ab4.css",
-  n + "/internal/immutable/nodes/0.5453729a.js",
-  n + "/internal/immutable/nodes/1.9b847bd7.js",
-  n + "/internal/immutable/nodes/10.6d086995.js",
-  n + "/internal/immutable/nodes/11.fa925b01.js",
-  n + "/internal/immutable/nodes/12.0a35754e.js",
-  n + "/internal/immutable/nodes/2.4fc6db23.js",
-  n + "/internal/immutable/nodes/3.4def5680.js",
-  n + "/internal/immutable/nodes/4.ebf2fd28.js",
+  n + "/internal/immutable/nodes/0.725f681f.js",
+  n + "/internal/immutable/nodes/1.ac2779b8.js",
+  n + "/internal/immutable/nodes/10.117e550d.js",
+  n + "/internal/immutable/nodes/11.446d836b.js",
+  n + "/internal/immutable/nodes/2.3fb2d588.js",
+  n + "/internal/immutable/nodes/3.40524fd0.js",
+  n + "/internal/immutable/nodes/4.265e15b1.js",
   n + "/internal/immutable/nodes/5.da5d2269.js",
-  n + "/internal/immutable/nodes/6.ad010fd0.js",
-  n + "/internal/immutable/nodes/7.884e375c.js",
-  n + "/internal/immutable/nodes/8.7a51f438.js",
-  n + "/internal/immutable/nodes/9.fd76e9d7.js",
+  n + "/internal/immutable/nodes/6.884e375c.js",
+  n + "/internal/immutable/nodes/7.7a51f438.js",
+  n + "/internal/immutable/nodes/8.fd76e9d7.js",
+  n + "/internal/immutable/nodes/9.4516a20b.js",
   n + "/internal/immutable/chunks/Dropdown.fc895c8c.js",
   n + "/internal/immutable/chunks/Icon.9bc2a10a.js",
   n + "/internal/immutable/chunks/Spinner.55af249c.js",
@@ -25,42 +24,42 @@ const n = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u
   n + "/internal/immutable/chunks/index.aac00523.js",
   n + "/internal/immutable/chunks/index.d7eb2526.js",
   n + "/internal/immutable/chunks/layout.c0462925.js",
-  n + "/internal/immutable/chunks/navigation.40667790.js",
+  n + "/internal/immutable/chunks/navigation.2416c48b.js",
   n + "/internal/immutable/chunks/notifications.48332ed3.js",
-  n + "/internal/immutable/chunks/paths.b340fad0.js",
+  n + "/internal/immutable/chunks/paths.3e620563.js",
   n + "/internal/immutable/chunks/pencil.2c400838.js",
   n + "/internal/immutable/chunks/scheduler.1587348c.js",
-  n + "/internal/immutable/chunks/singletons.1d58cdb6.js",
-  n + "/internal/immutable/chunks/stores.5d519baf.js",
+  n + "/internal/immutable/chunks/singletons.efcbd8bf.js",
+  n + "/internal/immutable/chunks/stores.93de8d05.js",
   n + "/internal/immutable/chunks/system.db9bdf7a.js",
   n + "/internal/immutable/chunks/user.5c70c963.js",
-  n + "/internal/immutable/entry/start.455b7904.js"
+  n + "/internal/immutable/entry/start.6254e58d.js"
 ], r = [
   n + "/favicon.png",
   n + "/icon.png",
   n + "/icon256x256.png",
   n + "/manifest.json",
   n + "/robots.txt"
-], o = "1700135869652", l = self, c = `cache-${o}`, m = [...u, ...r];
-l.addEventListener("install", (e) => {
-  async function t() {
-    await (await caches.open(c)).addAll(m);
+], o = "1700139850473", c = self, l = `cache-${o}`, m = [...u, ...r];
+c.addEventListener("install", (e) => {
+  async function a() {
+    await (await caches.open(l)).addAll(m);
   }
-  e.waitUntil(t());
+  e.waitUntil(a());
 });
-l.addEventListener("activate", (e) => {
-  async function t() {
-    for (const a of await caches.keys())
-      a !== c && await caches.delete(a);
+c.addEventListener("activate", (e) => {
+  async function a() {
+    for (const t of await caches.keys())
+      t !== l && await caches.delete(t);
   }
-  e.waitUntil(t());
+  e.waitUntil(a());
 });
-l.addEventListener("fetch", (e) => {
+c.addEventListener("fetch", (e) => {
   if (e.request.method === "GET") {
-    async function t() {
-      const a = new URL(e.request.url), s = await caches.open(c);
-      if (m.includes(a.pathname))
-        return s.match(a.pathname);
+    async function a() {
+      const t = new URL(e.request.url), s = await caches.open(l);
+      if (m.includes(t.pathname))
+        return s.match(t.pathname);
       try {
         const i = await fetch(e.request);
         return i.status === 200 && s.put(e.request, i.clone()), i;
@@ -68,6 +67,6 @@ l.addEventListener("fetch", (e) => {
         return s.match(e.request);
       }
     }
-    e.respondWith(t());
+    e.respondWith(a());
   }
 });
